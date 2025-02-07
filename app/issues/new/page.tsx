6c73ec0304ec1dda2296550d73084113d4e8baa5
ratnesh.p <ratnesh.p@media.net>
2025-02-07 19:16:53 +0530

    disable SSR

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React, { useState } from "react";
+import dynamic from "next/dynamic";
 import { TextField, Button, Callout } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
 import { useForm, Controller } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import axios from "axios";
@@ -11,6 +11,10 @@ import ErrorMessage from "@/app/components/ErrorMessage";
 import Loader from "@/app/components/Loader";
 import { zodResolver } from "@hookform/resolvers/zod";
 import "easymde/dist/easymde.min.css";
+
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
 
 type IssueForm = z.infer<typeof createIssueSchema>;
 
